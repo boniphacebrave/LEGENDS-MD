41c6a88050a98bcd65ca7ee24b1a2e0122fa4114
boniphacebrave <boniphacemalegesi7@gmail.com>
2025-08-21 13:07:50 +0300

    Update uptime.js

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -24,14 +24,14 @@ cmd({
     },
     message: {
       contactMessage: {
-        displayName: "CASEYRHODES | CASEYRHODES",
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:CASEYRHODES | CASEYRHODES-XMD\nORG:CASEYRHODES;\nTEL;type=CELL;type=VOICE;waid=254700000000:+254 700 000000\nEND:VCARD`,
+        displayName: "LEGENDS-MD | legend-md",
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:LEGEND| LEG-XMD\nORG:LEGEND-TECH;\nTEL;type=CELL;type=VOICE;waid=255794373476:+255 621 995 482\nEND:VCARD`,
         jpegThumbnail: Buffer.alloc(0)
       }
     }
   };
 
-  const uptimeText = `*🤖 CASEYRHODES-XMD Bot Uptime:*\n🕒 ${runtime()}\n\n💡 The bot has been running without interruption.`;
+  const uptimeText = `*🤖 LEGENDS-MD Bot Uptime:*\n🕒 ${runtime()}\n\n💡 The bot has been running without interruption.`;
 
   await Void.sendMessage(m.chat, {
     text: uptimeText,
@@ -39,17 +39,17 @@ cmd({
       forwardingScore: 999,
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: "120363302677217436@newsletter",
-        newsletterName: "CASEYRHODES-XMD Official"
+        newsletterJid: "120363419723191331@newsletter",
+        newsletterName: "LEGENDS-MD Official"
       },
       externalAdReply: {
-        title: "CASEYRHODES-XMD",
-        body: "Uptime Monitor by CASEYRHODES-XMD",
-        thumbnailUrl: "https://files.catbox.moe/51dcx2.jpg",
+        title: "LEGENDS-MD",
+        body: "Uptime Monitor by LEGENDS-MD",
+        thumbnailUrl: "https://files.catbox.moe/k07bn6.jpg",
         mediaType: 1,
         renderLargerThumbnail: true,
         showAdAttribution: true,
-        sourceUrl: "https://github.com/caseyweb"
+        sourceUrl: "https://github.com/boniphace"
       }
     }
   }, { quoted: fakeContact });
